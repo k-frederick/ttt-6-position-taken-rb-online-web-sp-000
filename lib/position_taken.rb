c9b2555cf7306340911f33b61be3ded987f7f06f
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-02 22:12:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  board[index] != " "
+  board[index] != " " board[index] != ""
   !board[0] == " "
   board[index] != "x" || board[index] != "o"
 end
