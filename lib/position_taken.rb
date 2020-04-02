146ead5e7f77fe48503c689a0aea637b36296fdc
Kristina Frederick <kristinafrederick@icloud.com>
2020-04-02 20:05:19 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
+require_relative '../lib/position_taken.rb'
+  
+end
+
 def position_taken?(board, index)
   if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     "false"
